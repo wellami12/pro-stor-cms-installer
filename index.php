4679a4338d47a53af2eb44226e8ecebc128fe57b
Valentin Kushchenko <well@pro-stor.net>
2017-06-18 14:46:08 +0300

    Add some default css and scripts for main and other future pages

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/app.css">
+    <title>PRO-STOR-CMS INSTALLER</title>
+</head>
+<body>
+
+<script src="js/jQuery.js"></script>
+<script src="js/tether.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/app.js"></script>
+</body>
+</html>
