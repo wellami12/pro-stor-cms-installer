33f736c773aca85abbbef2a5ccd113c6d6c8d734
Valentin Kushchenko <well@pro-stor.net>
2017-06-18 14:53:45 +0300

    Create Main Page(proc)

--- index.php
+++ index.php
@@ -8,7 +8,21 @@
     <title>PRO-STOR-CMS INSTALLER</title>
 </head>
 <body>
-
+<div class="container">
+    <div class="row">
+        <div class="offset-md-3 col-md-6">
+            <div class="card">
+                <img class="card-img-top" src="http://placehold.it/500x250" alt="Card image cap">
+                <div class="card-block">
+                    <h4 class="card-title">Card title</h4>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                        card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 <script src="js/jQuery.js"></script>
 <script src="js/tether.min.js"></script>
 <script src="js/bootstrap.min.js"></script>
